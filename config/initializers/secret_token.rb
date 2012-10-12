2a071b327254d299ed7fd751366bf3cb261e32d1
Edward Leung <EdwardLeung@localhost.(none)>
2012-10-12 10:36:01 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeepBlueSea::Application.config.secret_token = '23c0d8e43bbc3b24ea206fa0f5288198943dcdb6a0e0095512b3799d5ffdcaa9551c88f990da40645328db7c2fdb1d3041d7d53d06c22c3b1db75b2188032192'
